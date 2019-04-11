922528476a29a04aae66c28a41dd72f15c240200
lam-guo <824430898@qq.com>
2019-04-11 10:29:18 +0800

    香蕉君🍌

--- src/reminderView.ts
+++ src/reminderView.ts
@@ -9,26 +9,28 @@ export class ReminderView {
         if (this.panel) {
             this.panel.reveal();
         } else {
-            this.panel = vscode.window.createWebviewPanel("ycy", "杨超越", vscode.ViewColumn.Two, {
+            this.panel = vscode.window.createWebviewPanel("banana", "香蕉君", vscode.ViewColumn.Two, {
                 enableScripts: true,
                 retainContextWhenHidden: true,
             });
 
-            const imagePath = vscode.Uri.file(path.join(context.extensionPath, 'images', 'ycy0.png'))
+            const imagePath = vscode.Uri.file(path.join(context.extensionPath, 'images', 'banana.jpg'))
                 .with({ scheme: 'vscode-resource' });
 
-            this.panel.webview.html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>杨超越</title>
-</head>
-<body>
-    <div><h1>小哥哥~ 代码写久了，该休息啦~</h1></div>
-    <div><img src="${imagePath}"></div>
-</body>
-</html>`;
+            this.panel.webview.html = `
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>香蕉君</title>
+                </head>
+                <body>
+                    <div><h1>小老弟~ 代码写久了，往后稍稍~</h1></div>
+                    <div><img src="${imagePath}"></div>
+                </body>
+            </html>
+            `;
 
             this.panel.onDidDispose(() => {
                 this.panel = undefined;
